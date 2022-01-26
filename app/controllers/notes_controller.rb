68504d49b5caa867bca65fb54ddc74848797bd4b
Roger Campos <roger@rogercampos.com>
2022-01-26 23:12:15 +0100

    V2: delete+create on streams

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,11 +2,12 @@ class NotesController < ApplicationController
   def create
     @note = Note.new params[:note].permit!
 
-    if @note.save
-      redirect_to root_path
-    else
-      @notes = Note.all
-      render :index, status: :unprocessable_entity
-    end
+    @result = @note.save
+  end
+
+  def destroy
+    @note = Note.find params[:id]
+
+    @note.destroy!
   end
 end
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,3 @@
 class Note < ApplicationRecord
-  validates :title, presence: true
+  validates :title, presence: true, length: { minimum: 5}
 end
